46daa06f0f5c4c6c52cb5f35980f15e6fb1d82ae
Cristian121227 <144190435+Cristian121227@users.noreply.github.com>
2023-11-23 19:19:54 +0000

    Routes added

--- example/urls.py
+++ example/urls.py
@@ -3,8 +3,51 @@ from django.urls import path
 
 from example.views import index
 
-
 urlpatterns = [
     path('', index),
-    path('hola', index)
+    path('hola', index),
+]
+
+
+
+
+
+
+
+# example/urls.py
+from django.urls import path
+
+from example.views import (
+    index,
+    not_found,
+    blank,
+    buttons,
+    cards,
+    charts,
+    forgot_password,
+    user_login,
+    register,
+    tables,
+    utilities_animation,
+    utilities_border,
+    utilities_color,
+    utilities_other,
+)
+
+urlpatterns = [
+    path('index.html', index),
+    path('hola', index),
+    path('404.html', not_found),
+    path('blank.html', blank),
+    path('buttons.html', buttons),
+    path('cards.html', cards),
+    path('charts.html', charts),
+    path('forgot-password.html', forgot_password),
+    path('login.html', user_login),
+    path('register.html', register),
+    path('tables.html', tables),
+    path('utilities-animation.html', utilities_animation),
+    path('utilities-border.html', utilities_border),
+    path('utilities-color.html', utilities_color),
+    path('utilities-other.html', utilities_other),
 ]
